2a80329b910ffdbd3609d1e47ecd9c2d8e0a88ab
DyDragon16 <duyle1612003@gmail.com>
2024-07-01 11:25:01 +0700

    udate ne Hai

--- src/main/java/com/hutech/demo/controller/CountryController.java
+++ src/main/java/com/hutech/demo/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.hutech.demo.controller;
 import com.hutech.demo.NotFoundByIdException;
 import com.hutech.demo.model.Country;
 import com.hutech.demo.service.CountryService;
+import com.hutech.demo.service.RegionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +20,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class CountryController {
     @Autowired
     private final CountryService countryService;
+    @Autowired
+    private final RegionService regionService;
 
     @GetMapping("/countries/add")
     public String showFormAdd(Model model)
     {
         model.addAttribute("country",new Country());
+        model.addAttribute("regions", regionService.getAllRegions());
         return "country/add-country"; // corrected the path
     }
     @PostMapping("/countries/add")
@@ -49,6 +53,7 @@ public class CountryController {
         try {
             Country country =  countryService.getByIdCountry(id);
             model.addAttribute("country", country);
+            model.addAttribute("regions", regionService.getAllRegions());
             return "/country/edit-country";
         } catch (NotFoundByIdException e) {
             return "redirect:/countries";
